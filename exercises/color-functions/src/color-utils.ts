bee053380c5a974e28fe32f687c712090b7ed471
andino <andy.andino93@gmail.com>
2018-02-15 15:05:58 -0800

    finish color-utils

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,22 @@
 //TODO: Implement hexToRgb
-
+export const hexToRgb = (hex: string): {r: number, g: number, b:number} => {
+  if (hex.length === 3) {
+    let hex6 = ''
+    for (let hexCh of hex) {
+      hex6 += (hexCh + hexCh)
+    }
+    return hexToRgb(hex6)
+  }
+  const [r, g, b] = [0, 2, 4]
+    .map(offset => hex.substring(offset, offset + 2))
+    .map(hexCh => parseInt(hexCh, 16))
+  return { r, g, b }
+}
 
 //TODO: Implement rgbToHex
+export const rgbToHex = (r: number, g: number, b: number): string => {
+  return [r, g, b]
+    .map(decCh => Math.max(0, Math.min(255, decCh)).toString(16))
+    .map(hexCh => hexCh.length === 1 ? `${hexCh}${hexCh}` : hexCh)
+    .join('')
+}
